3a507b33216fc0e9a669c47caf8b1e68c9466c38
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-11-20 01:04:40 +0600

    uppdate

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,57 @@
 $(document).ready(function(){
 	// Mobile Menu
-	// $('.logo a i').click(function(){
-	// 	$('.menu ul').slideToggle(1000);
-
-	// 	return false
-	// });
+	$(document).ready(function () {
+		$('.bars_icon').click(function (e) {
+			e.preventDefault();  
+	 
+			$('.bars_menu').slideToggle(1000);
+			$(this).toggleClass('fixed_bars');
+	 
+			const icon = $(this).find('i');
+			if (icon.hasClass('fa-bars')) {
+				icon.removeClass('fa-bars').addClass('fa-times');  
+			} else {
+				icon.removeClass('fa-times').addClass('fa-bars');
+			}
+		});
+	});
+	
 
 	// Banner Carousel
-	// $('.banner_carousel').owlCarousel({
-	// 	items:1,
-	// 	loop:true,
-	// 	nav:true,
-	// 	dots:true,
-	// 	autoplay:true,
-	// });
+	$(document).ready(function(){
+		$(".banner_carousel").owlCarousel({
+			items: 1,
+			loop: true,
+			autoplay: true,
+			nav: false,
+			touchDrag: false,
+			mouseDrag: false, 
+			dots: false,
+			autoplayTimeout: 6000, 
+			smartSpeed: 3000, 
+			animateOut: 'fadeOut',
+			animateIn: 'fadeIn',
+			onTranslated: function(event) {
+				$(".banner_carousel_item").removeClass("active");  
+				$(".owl-item.active .banner_carousel_item").addClass("active");  
+			},
+			onInitialized: function(event) {
+				$(".owl-item.active .banner_carousel_item").addClass("active");  
+			}
+		});
+	});
+
+	$(document).ready(function () {
+		$(window).on("scroll", function () { 
+			if ($(window).scrollTop() > 0) {
+				$(".bars_icon").addClass("fixed_bars");
+			} else {
+				$(".bars_icon").removeClass("fixed_bars");
+			}
+		});
+	});
+	
+	
 
 
 
