b33a485c3eba1a71e99200f8ae6626d34e38f0c7
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-11-21 17:30:19 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,36 @@
+$(document).ready(function () {
+    $(".ex_banner_carousel").owlCarousel({
+        items: 1,                // Show one item at a time
+        loop: true,              // Enable infinite looping
+        autoplay: true,          // Enable autoplay
+        nav: false,              // Disable navigation arrows
+        dots: true,              // Show navigation dots
+        autoplayTimeout: 6000,   // 6 seconds per slide
+        smartSpeed: 1000,        // Smooth transition speed
+        onTranslated: function () {
+            // Remove animation from all content
+            $(".image_content").removeClass("animate-content");
+            // Add animation to the active content
+            $(".owl-item.active .image_content").addClass("animate-content");
+        },
+        onInitialized: function () {
+            // Add animation to the first visible content
+            $(".owl-item.active .image_content").addClass("animate-content");
+        }
+    });
+});
+
+
+
+
+
 $(document).ready(function(){
 	// Mobile Menu
 	$(document).ready(function () {
 		$('.bars_icon').click(function (e) {
 			e.preventDefault();  
 	 
-			$('.bars_menu').slideToggle(1000);
-			
-			if (!$(this).hasClass('fixed_bars')) {
-				$(this).addClass('fixed_bars');
-			}
+			$('.bars_menu').slideToggle(1000);  
 	 
 			const icon = $(this).find('i');
 			if (icon.hasClass('fa-bars')) {
@@ -43,13 +65,26 @@ $(document).ready(function(){
 			}
 		});
 	});
+ 
+ 
+	
+	
+	
 
 	$(document).ready(function () {
 		$(window).on("scroll", function () { 
 			if ($(window).scrollTop() > 0) {
-				$(".bars_icon").addClass("fixed_bars");
+				// $(".bars_icon").addClass("fixed_bars");
+				$('.header_area').addClass('header_fixed');
+				$('.header_area').removeClass('extra_header');
+				$('.logo').css({'opacity':'0','visibility':'hidden'});
+				$('.hide_menu').css({'opacity':'0','visibility':'hidden'});
 			} else {
-				$(".bars_icon").removeClass("fixed_bars");
+				$('.header_area').removeClass('header_fixed');
+				// $(".bars_icon").removeClass("fixed_bars");
+				$('.header_area').addClass('extra_header');
+				$('.logo').css({'opacity':'1','visibility':'visible'});
+				$('.hide_menu').css({'opacity':'1','visibility':'visible'});
 			}
 		});
 	});
